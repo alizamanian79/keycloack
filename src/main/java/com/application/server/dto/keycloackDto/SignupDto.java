b1ddd8bf29f072c27d61c25ca7b728b9c95837b8
Ali <120646614+alizamanian79@users.noreply.github.com>
2025-01-19 08:58:04 +0330

    Update SignupDto.java

--- src/main/java/com/application/server/dto/keycloackDto/SignupDto.java
+++ src/main/java/com/application/server/dto/keycloackDto/SignupDto.java
@@ -1,10 +1,6 @@
 package com.application.server.dto.keycloackDto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 public class SignupDto extends SigninDto {
     private Boolean enabled; // Fixed spelling from 'enabaled' to 'enabled'
